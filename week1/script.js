69448905d1eec0ff0bca786ae8bd46351f4f8a91
nv-tanh <nvtanh01@gmail.com>
2019-07-18 19:58:06 +0700

    Milestone 5
     Different Currencies.

--- week1/script.js
+++ week1/script.js
@@ -4,4 +4,7 @@ function convertToVnd() {
     let vndAmount = amount*conversion;
     let result = document.getElementById('result');
     result.innerHTML = "50 USD is " + vndAmount + " VND"
+
+    let currencyType = document.querySelector('input[name="currency"]:checked').value;
+
 }
