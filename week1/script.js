be3affe208c9625e59470a10d11e9cedec82c1cc
Khanh Tran-Cong <congkhanh197@gmail.com>
2019-07-18 20:05:06 +0700

    Add call API

--- week1/script.js
+++ week1/script.js
@@ -1,6 +1,24 @@
 function convertToVnd() {
+    callApi('USD');
     let conversion = 23262;
     let vndAmount = 50;
     let result = document.getElementById('result');
     result.innerHTML = vndAmount
+}
+function callApi(currency) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://free.currencyconverterapi.com/api/v6/convert?q=' + currency + '_VND&compact=ultra&apiKey=1c898f56dbb0dcde1c3e');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            updateResults(JSON.parse(xhr.responseText));
+        }
+        else {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
+}
+  
+function updateResults(response) {
+    console.log(response['USD_VND']);
 }
