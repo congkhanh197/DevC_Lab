04f53934cc563c62354d297fd8a653de75dd2491
nv-tanh <nvtanh01@gmail.com>
2019-07-18 19:49:26 +0700

    Milestone 4
    Add a textbox.

--- week1/script.js
+++ week1/script.js
@@ -1,5 +1,6 @@
 function convertToVnd() {
     let conversion = 23262;
-    let vndAmount = 50;
+    let amount = document.getElementById("amount").value;
+    let vndAmount = amount*conversion;
     document.write("50 USD is " + vndAmount + " VND");
 }
